2f62478d93116d1cfcde0b38a525d6f557c944bd
Peter Williams <retex.williams@gmail.com>
2017-12-18 11:26:42 +0000

    added routes and views

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+
+const app = express();
+
+// set up view engine
+app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
+// Middleware
+app.use('/assets', express.static('assets'));
+// Create home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, ()=> {
+  console.log('App now listening for requests on port 3000');
+});
