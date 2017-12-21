2a153b8b179e6857c405233ba6b17176b719ed72
Peter Williams <retex.williams@gmail.com>
2017-12-21 22:38:38 +0000

    updated functionality - fleshed out google strategy

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+
+// connect to mongodb (throws deprecation errors)
+// mongoose.connect(keys.mongodb.dbURI, () => {
+//   console.log('connected to mongodb');
+// });
+
+mongoose.connection.openUri(keys.mongodb.dbURI, () => {
+  console.log('connected to mongodb');
+});
+
 // set up routes
 app.use('/auth', authRoutes);
-// Middleware
+// Middleware for css
 app.use('/assets', express.static('assets'));
 // Create home route
 app.get('/', (req, res) => {
