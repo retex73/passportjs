1657b9ff22dbe776152ae0ce4e9b5a764154516f
Peter Williams <retex.williams@gmail.com>
2017-12-21 23:25:08 +0000

    updated functionality - fleshed out google strategy

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+// cookie session
+app.use(cookieSession({
+  maxAge: 24*60*60*1000,
+  keys: [keys.session.cookieKey]
+}));
+
+// Initialise passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb (throws deprecation errors)
 // mongoose.connect(keys.mongodb.dbURI, () => {
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-models');
 
+passport.serializeUser((user, done)=> {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=> {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy({
   // options for the google strategy
@@ -15,6 +25,7 @@ passport.use(
       if(currentUser){
         // already have the user
         console.log('user is:', currentUser);
+        done(null, currentUser);
       } else {
         // if not, create user in our db
         new User({
@@ -22,6 +33,7 @@ passport.use(
           googleId: profile.id
         }).save().then((newUser) => {
           console.log('new user created:' + newUser);
+          done(null, newUser);
         });
       }
     })
